0bc07c85e78c833f3250b53f7d2443a7854a55b9
abraspin <abrahamspindel@gmail.com>
2021-03-11 16:02:58 -0600

    remove react boilerplate from home page

---
+++ client/src/pages/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Search() {
+
+    return (
+
+        <div>
+            <h1>Hi this is the budget app home page</h1>
+        </div>
+    )
+}
+export default Search;
